3410da50132f962ff5908805fa0d63f78b8b8de1
abelitoe <abelmayuri01@gmail.com>
2024-10-21 20:01:04 +0700

    cdlb3

---
+++ src/cdlb1/main.java
@@ -0,0 +1,17 @@
+package cdlb1;
+
+class hitung {
+    int a = 1;
+    int b = 3;
+
+    int jumlah = a + b ;
+}
+
+public class main {
+    public static void main(String[] args) {
+        hitung obj = new hitung();
+        System.out.println("Jumlah : " + obj.jumlah);
+
+    }
+
+}
